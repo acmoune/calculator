fe09955466ab638897b41e41b26349ec9dc3a3c1
Chris Moune <ac.moune@gmail.com>
2020-12-18 12:03:33 +0100

    fixed hazelcast

--- src/main/java/cm/digitalhub/calculator/Calculator.java
+++ src/main/java/cm/digitalhub/calculator/Calculator.java
@@ -1,12 +1,12 @@
 package cm.digitalhub.calculator;
 
-import org.springframework.cache.annotation.Cacheable;
+// import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Calculator {
 
-  @Cacheable("sum")
+  // @Cacheable("sum")
   public int sum(int a, int b) {
     /*
     try {
