cde293544ccb923582f049185deb9eb7719660d0
Chris Moune <ac.moune@gmail.com>
2020-12-18 11:12:09 +0100

    Add Kubernetes support

--- src/main/java/cm/digitalhub/calculator/CalculatorApplication.java
+++ src/main/java/cm/digitalhub/calculator/CalculatorApplication.java
@@ -1,13 +1,27 @@
 package cm.digitalhub.calculator;
 
+import com.hazelcast.client.config.ClientConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableCaching
 public class CalculatorApplication {
+
+  @Value("${hazelcast.host}")
+  private String hazelcastHost;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
 
+	@Bean
+  public ClientConfig hazelcastClientConfig() {
+    var clientConfig = new ClientConfig();
+    clientConfig.getNetworkConfig().addAddress(hazelcastHost);
+    return clientConfig;
+  }
 }
